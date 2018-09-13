471e8ae1eaa47747541719ae45d76a0ca1571306
unknown <zhangwencheng@xiaomi.com>
2018-09-13 22:13:07 +0800

    添加leetcode

---
+++ climb_stairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<1:
+            return 0
+        Solution.stairs = [1,2]
+        Solution.result = []
+        self.DFS(n, 0, [])
+        return len(Solution.result)
+    def DFS(self, target, i, lst):
+        if target == 0 and lst not in Solution.result:
+            Solution.result.append(lst)
+        if target-1>=0:
+            self.DFS(target-1, 0, lst+[1])
+        if target-2>=0:
+            self.DFS(target-2, 0, lst+[2])
+
+if __name__ == "__main__":
+    print Solution().climbStairs(10)
--- remove_element.py
+++ remove_element.py
@@ -1,20 +1,19 @@
-class Solution():
-    def removeElement(nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        numbers = 0
-        if nums == []:
-            return numbers
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[numbers] = nums[i]
-                numbers += 1
-        return numbers
+def removeElements(head, val):
+    """
+    :type head: ListNode
+    :type val: int
+    :rtype: ListNode
+    """
+    while head is not None and head.val == val:
+        head = head.next
+    p = head
+    while p is not None and p.next is not None:
+        while p.next is not None and p.next.val == val:
+            p.next = p.next.next
+        p = p.next
+    return head
 
-    if __name__ == "__main__":
-        nums = [0, 1, 2, 2, 3, 0, 4, 2]
-        val = 2
-        print removeElement(nums,val)
+if __name__ == "__main__":
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    val = 2
+    print removeElements(nums,val)
